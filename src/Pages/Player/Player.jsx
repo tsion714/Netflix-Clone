868f89083254b4e7a2d9ace1106cba27774718af
Tsion <tsion714@gmail.com>
2025-06-19 14:47:55 -0400

    update

--- src/Pages/Player/Player.jsx
+++ src/Pages/Player/Player.jsx
@@ -9,12 +9,13 @@ const Player = () => {
   const navigate = useNavigate();
 
   const[apiData, setApiData] = useState({
-    name:"",
-    key:"",
-    published_at:"",
-    typeof:""
+    name: "",
+    key: "",
+    published_at: "",
+    type: ""
   })
 
+  
   const options = {
     method: 'GET',
     headers: {
@@ -28,16 +29,16 @@ const Player = () => {
     .then(res => res.json())
     .then(res => setApiData(res.results[0]))
     .catch(err => console.error(err));
-  },[])
- 
+  },[id])
 
+   
   return (
     <div className='player'>
       <img src={back_arrow_icon} alt="" onClick={() => {navigate(-2)}}/>
       <iframe width='90%' height='90%'
       src={`https://www.youtube.com/embed/${apiData.key}`} 
       title='trailer'
-      frameborder='0'
+      frameBorder='0'
       allowFullScreen></iframe>
       <div className="player-info">
         <p>{apiData.published_at.slice(0,10)}</p>
